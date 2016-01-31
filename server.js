27792007cece1fc6040a95ded98e37c13b2161f0
aboone08 <andranique.boone@gmail.com>
2016-01-31 00:48:43 -0500

    made additions to the MEAN Temlate for Twitter page attempt

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
 });
 
+server.post('/signup',function(req, res){
+  console.log("You signed up!");
+  res.redirect('/');
+})
+
+server.use(express.static(__dirname+"/css"));
+
 server.listen(8080, function(){
 	console.log("Now listening on port 8080");
 });
